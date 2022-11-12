6723d7b771a333203eafbfcd2f27063880d816f7
KTo <kto@lovetou.ru>
2022-11-12 17:04:31 +0700

    Добавил мультипоточность

--- lesson-01/task_01.py
+++ lesson-01/task_01.py
@@ -4,13 +4,24 @@
 # («Узел доступен», «Узел недоступен»). При этом ip-адрес сетевого узла должен создаваться с помощью функции ip_address().
 import socket
 import platform
+import threading
+import time
 
 from threading import Thread
 from ipaddress import ip_address
 from subprocess import Popen, PIPE
 
 
+def host_ping_thread(ip_addr, result):
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    args = ['ping', param, '2', str(ip_addr)]
+    reply = Popen(args, stdout=PIPE, stderr=PIPE)
+
+    code = reply.wait()
+    result['lucky' if code == 0 else 'unlucky'].append(ip_addr)
+
 def host_ping(address_list):
+    threads = []
     result = {'lucky':[], 'unlucky': []}
     for address in address_list:
         try:
@@ -22,17 +33,28 @@ def host_ping(address_list):
                 result['unlucky'].append(address)
                 continue
 
-        param = '-n' if platform.system().lower() == 'windows' else '-c'
-        args = ['ping', param, '2', str(ip_addr)]
-        reply = Popen(args, stdout=PIPE, stderr=PIPE)
+        thread = threading.Thread(target=host_ping_thread, args=(ip_addr, result), daemon=True)
+        thread.start()
+        threads.append(thread)
 
-        code = reply.wait()
-        result['lucky' if code == 0 else 'unlucky'].append(ip_addr)
+    while True:
+        enough = True
+        time.sleep(1)
+        for thread in threads:
+            if thread.is_alive():
+                enough = False
+                break
+
+        if not enough:
+            continue
+        break
 
     return result
 
 
 if __name__ == '__main__':
-    # address_list = ['yandex.ru', '127.0.0.1', 'no host']
-    address = input('Введите адрес или имя хоста: ')
-    print(host_ping([address]))
+    address_list = ['yandex.ru', '127.0.0.1', 'no host', 'lovetou.ru']
+    print(host_ping(address_list))
+
+    # address = input('Введите адрес или имя хоста: ')
+    # print(host_ping([address]))
