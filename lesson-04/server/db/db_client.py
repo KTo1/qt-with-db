e7d3a2bc5d9ddf4720bd081d83ca8f2a70def680
KTo <kto@lovetou.ru>
2022-11-22 21:12:02 +0700

    Волевым решением разделил приложения клиента и сервера

---
+++ lesson-04/server/db/db_client.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from db_connect import Base
+
+
+class DbClient(Base):
+    __tablename__ = 'clients'
+
+    id = Column(Integer, primary_key=True)
+    login = Column(String)
+    info = Column(String)
+
+    def __init__(self, login, info):
+        self.login = login
+        self.info = info
+
+    def __repr__(self):
+        return f'id: {self.id}, login>: {self.login}, last login: {self.info}'
